fab58fd7c1bb404afa4a20e191e48e62004e78b2
jumakiwaka <alijuma232@gmail.com>
2020-05-29 01:41:52 +0300

    fix site responsiveness

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,29 +1,30 @@
-
-
 const appState = {
-    url: 'ratings.html',
-}
+  url: "ratings.html",
+};
 
 const popUpForm = () => {
-    const newWindow = open('/ratings.html', 'Rate', 'width=200,height=200,location=0,toolbar=no,menubar=no,status=no,resizable=no,titlebar=no');
-    newWindow.focus();
-    newWindow.document.querySelector('button').addEventListener('click', () => {
-        newWindow.close();
-    })
-}
+  const newWindow = open(
+    "/ratings.html",
+    "Rate",
+    "width=200,height=200,location=0,toolbar=no,menubar=no,status=no,resizable=no,titlebar=no"
+  );
+  newWindow.focus();
+  newWindow.document.querySelector("button").addEventListener("click", () => {
+    newWindow.close();
+  });
+};
 
-document.querySelector('.intro button').addEventListener('click', () => {
-    popUpForm();
-});
+// document.querySelector('.intro button').addEventListener('click', () => {
+//     popUpForm();
+// });
 
 const toggleAbout = (e) => {
-    const div = document.querySelector('.about-me + div');
-    div.style.display = div.style.display === 'none' ? 'block' : 'none';
-}
+  const div = document.querySelector(".about-me + div");
+  div.style.display = div.style.display === "none" ? "block" : "none";
+};
 
-const btn = document.querySelector('.about-me > button');
+const btn = document.querySelector(".about-me > button");
 
-btn.addEventListener('click', (e) => {
-    toggleAbout(e);
+btn.addEventListener("click", (e) => {
+  toggleAbout(e);
 });
-
